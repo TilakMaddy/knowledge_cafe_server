007c066a18cd6482aeea083d1fe2e552feb3d252
TilakMaddy <tilak.madichetti@gmail.com>
2023-06-18 15:54:24 +0530

    alpha test done

--- index.js
+++ index.js
@@ -1,9 +1,68 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-const server = http.createServer((req, res) => {
-  res.status(200).send("Hello")
+app.use(cors());
+
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+let connected = 0;
+
+io.on('connection', (socket) => {
+  connected++;
+  console.log(`${connected} people online !`)
+  io.emit('ONLINE', connected);
+
+  socket.on('disconnect', () => {
+    connected--;
+    console.log(`${connected} people are online !`)
+    io.emit('ONLINE', connected);
+  })
+
+  // Identify the connected entity based on the socket's emit event message
+
+  // In case of organizer, event can be
+
+  // 1. ORG SHOW <X>  ``` where X is the question number
+  //      -> Respond by emitting NODE SHOW <X> for the audience to listen
+
+  // 2. ORG HIDE
+  //      -> Respond by emitting NODE HIDE for the audience to listen
+
+  socket.on('ORG SHOW', (question) => {
+    io.emit('NODE SHOW', question);
+  });
+
+  socket.on('ORG HIDE', () => {
+    io.emit('NODE HIDE');
+  });
+
+  // In case of audience, event will be
+  // AUD <USERNAME> <X> <OPTION>
+  //      -> Respond by emitting NODE ANS for organizer to listen
+
+  socket.on('AUD', (data) => {
+    console.log(data);
+
+    /*const user = data.user
+        const questionNumber = data.questionNumber;
+        const answer = data.answer;*/
+
+    io.emit('NODE ANS', data)
+  })
+
+
 });
+
+const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
